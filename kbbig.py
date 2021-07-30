a1ce165d2141da8e5e9f87853f412db61a062cae
Bruna Andrade <81596214+nanellide@users.noreply.github.com>
2021-07-30 09:50:30 -0300

    Add files via upload

--- kbbig.py
+++ kbbig.py
@@ -13,27 +13,21 @@ bard_instruments = ['accordion', 'balalaika', 'beer bottles', 'bells', 'bodhrán
 'musical saw', 'pan flute', 'psaltery', 'renaissance guitar', 'sitar', 'standing bell', 'timpani', 'tin whistle',
 'tupan', 'uilleann pipes', 'valiha', 'whistle']
 
-# Now for the generator... Simple input.
-gen = input("""
+# Now for the generator... Simple input followed by a function that
+print("""
 Welcome to the Kate Bush Bard Instrument Generator (KBBIG)! With over 30 options, the KBBIG is directly inspired by Kate Bush's discography and her love of experimental instruments.
 
-Simply hit ENTER on your keyboard and it will generate an unconventional/rare instrument for your bard to specialize in!\n""")
+Simply hit ENTER on your keyboard and it will generate an unconventional/rare instrument for your bard to specialize in!""")
 
-# And that's it! There's probably a better way to do this that does not involve calling a
-# new function again............. but I'm lazy and this works.
-chosen_instrument = random.choice(bard_instruments)
-
-print(f"Your bard's brand new instrument is...... THE", chosen_instrument.upper() + "!")
-print("\nDon't like your instrument? Hit ENTER to generate another one or quit this script by pressing Q.")
-
-def another(question):
-    prompt2 = (f"{question}")
-    run2 = (input(prompt2).lower().strip())
-    while run2 not in ['q']:
+# And that's it! There's probably a better way to do this, but I'm lazy and this works.
+def gen(int):
+    prompt = (f"{int}")
+    run = (input(prompt).lower().strip())
+    while run not in ['q']:
         print("\nYour bard's brand new instrument is...... THE", random.choice(bard_instruments).upper() + "!")
         print("\nDon't like your instrument? Hit ENTER to generate another or quit this script by pressing Q.")
-        return another(question)
-    if run2 == 'q':
+        return gen(int)
+    if run == 'q':
         return print("\nThank you for using KBBIG!"), sys.exit()
 
-run2 = another(">")
+run = gen(">")
