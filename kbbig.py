86a67d60846f81cd76906f2ce0cbf0bdb1ec0412
Bruna Andrade <81596214+nanellide@users.noreply.github.com>
2021-07-30 09:27:15 -0300

    Add files via upload

--- kbbig.py
+++ kbbig.py
@@ -13,54 +13,25 @@ bard_instruments = ['accordion', 'balalaika', 'beer bottles', 'bells', 'bodhrán
 'musical saw', 'pan flute', 'psaltery', 'renaissance guitar', 'sitar', 'standing bell', 'timpani', 'tin whistle',
 'tupan', 'uilleann pipes', 'valiha', 'whistle']
 
-print("Welcome, traveler! This KBBIG script will require you to answer a few questions first.")
-print("\nPress Y to answer YES and N to answer NO, then hit ENTER to confirm.")
-
-# Simple yes or no function that prompts user to type two possible answers.
-# A third answer will loop the program back on itself.
-def yesno(question):
-    prompt = (f"{question}")
-    reply = (input(prompt).lower().strip())
-    while reply not in ['y', 'n']:
-        print("\nOh my god, you had ONE job. Come on, read the previous lines more carefully and try again.")
-        return yesno(question)
-        # Honestly, I could keep this one going forever but I do not want to torture my user.
-    if reply == "y":
-        return True
-    if reply == "n":
-        return print("\nOh, okay then. I guess you're not the target demographic anyway."), sys.exit()
-
-# Prompt questions!
-print("\nDo you like playing tabletop RPG as a bard?")
-reply = yesno(">")
-
-print("\nTired of the same old bard instruments like fiddle or flute?")
-reply = yesno(">")
-
-print("\nIs your DM rolling his eyes as you tell him you'll be playing a bard? AGAIN?")
-reply = yesno(">")
-
-# Now for the generator... Simple input this time.
+# Now for the generator... Simple input.
 gen = input("""
-Break the chain with this brand new Kate Bush Bard Instrument Generator! With over 30 options, the KBBIG is directly inspired by Kate Bush's discography and her love of experimental instruments.
+Welcome to the Kate Bush Bard Instrument Generator (KBBIG)! With over 30 options, the KBBIG is directly inspired by Kate Bush's discography and her love of experimental instruments.
 
 Simply hit ENTER on your keyboard and it will generate an unconventional/rare instrument for your bard to specialize in!\n""")
 
-# And that's it! Now for the reveal and loop:
+# And that's it! There's probably a better way to do this that does not involve calling a
+# new function again............. but I'm lazy and this works.
 chosen_instrument = random.choice(bard_instruments)
 
 print(f"Your bard's brand new instrument is...... THE", chosen_instrument.upper() + "!")
-print("\nDon't like your instrument? Hit R to generate another one or Q to quit this script.")
+print("\nDon't like your instrument? Hit ENTER to generate another one or quit this script by pressing Q.")
 
 def another(question):
     prompt2 = (f"{question}")
     run2 = (input(prompt2).lower().strip())
-    while run2 == 'r':
+    while run2 not in ['q']:
         print("\nYour bard's brand new instrument is...... THE", random.choice(bard_instruments).upper() + "!")
-        print("\nDon't like your instrument? Hit R to generate another one or Q to quit this script.")
-        return another(question)
-    if run2 not in ['r', 'q']:
-        print("\nLike I said, hit R for another instrument or Q to quit. Take it or leave it, sweetie.")
+        print("\nDon't like your instrument? Hit ENTER to generate another or quit this script by pressing Q.")
         return another(question)
     if run2 == 'q':
         return print("\nThank you for using KBBIG!"), sys.exit()
